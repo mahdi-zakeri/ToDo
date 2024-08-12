4779ef20859fcf2d9066cc0ef6b1c86c6a7200cf
mhadi <m.mahdizakeri@gmail.com>
2024-08-12 16:00:51 +0330

    feat: add background

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <></>
+      <div className="h-screen bg-gradient-to-br from-fuchsia-950 from-50% via-0% to-rose-500 to-50%">
+        {/* Your app content here */}
+      </div>
   )
 }
 
