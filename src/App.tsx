5a5a8aefab5493988b23c64a3406ab3de405f91e
mhadi <m.mahdizakeri@gmail.com>
2024-08-14 10:36:27 +0330

    feat: add main card

--- src/App.tsx
+++ src/App.tsx
@@ -22,15 +22,50 @@ function App() {
 
 
     return (
-      <div className="h-screen bg-gradient-to-br from-fuchsia-950 from-50% via-0% to-rose-500 to-50% flex justify-center items-center ">
-          <button onClick={addTask} className="bg-amber-50 p-4 rounded-md flex-">Add Task</button>
-          {tasks.map((task: Task) => (
-              <p id="font">
-                  {task.name}
-              </p>
-          ))}
-      </div>
-  )
+        <div
+            className="min-h-screen bg-gradient-to-br from-fuchsia-950 from-50% via-0% to-rose-500 to-50% flex justify-center items-center">
+            {/*<button onClick={addTask} className="bg-amber-50 p-4 rounded-md flex-">Add Task</button>*/}
+            <div className="bg-white shadow-xl rounded-lg overflow-hidden">
+                <div className="h-1/5 bg-yellow-300 bg-cover bg-center p-4">
+                    <h1 className="justify-end text-fuchsia-950 w-fit mx-auto p-2 pb-4">To-Do List</h1>
+                    <div className="flex ">
+                        <input
+                            className="bg-transparent border-b border-fuchsia-950 placeholder-fuchsia-950 select focus:outline-none text-fuchsia-950 w-full"
+                            placeholder="Hello World"/>
+                        <button className="text-white rounded-full ml-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40"
+                                 viewBox="0,0,256,256">
+                                <g fill="#4c224a" >
+                                    <g transform="scale(5.12,5.12)">
+                                        <path
+                                            d="M25,2c-12.683,0 -23,10.317 -23,23c0,12.683 10.317,23 23,23c12.683,0 23,-10.317 23,-23c0,-12.683 -10.317,-23 -23,-23zM37,26h-11v11h-2v-11h-11v-2h11v-11h2v11h11z"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div className="h-4/5 p-4">
+                    <div>
+                        <h2>
+                            <span className="underline decoration-4 decoration-fuchsia-950 ">To</span>-Do
+                        </h2>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <h2>
+                            <span className="underline decoration-4 decoration-fuchsia-950 ">D</span>one
+                        </h2>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
